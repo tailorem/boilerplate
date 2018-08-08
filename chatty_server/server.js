2845b12137a58fc30148fdc1dcfbc2e2e6285334
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-08 16:07:39 -0700

    tidies server code

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -34,8 +34,6 @@ const handleMessageFromClient = (message) => {
   message.id = uuidv4();
   newMessage = JSON.stringify(message);
   broadcastMessageFromClient(newMessage);
-
-  // console.log('User', message.username, 'said', message.content);
 };
 
 wss.on('connection', (ws) => {
@@ -47,25 +45,4 @@ wss.on('connection', (ws) => {
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
-
-
-// // Broadcast to all.
-// wss.broadcast = function broadcast(data) {
-//   wss.clients.forEach(function each(client) {
-//     if (client.readyState === WebSocket.OPEN) {
-//       client.send(data);
-//     }
-//   });
-// };
-
-// wss.on('connection', function connection(ws) {
-//   ws.on('message', function incoming(data) {
-    // Broadcast to everyone else.
-                // wss.clients.forEach(function each(client) {
-                //   if (client !== ws && client.readyState === WebSocket.OPEN) {
-                //     client.send(data);
-                //   }
-//     });
-//   });
-// });
 
