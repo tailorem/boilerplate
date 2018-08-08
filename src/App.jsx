e2691e23b9c7096361124738b234adbdfaec923e
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-07 20:27:22 -0700

    adds websocket to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,19 +37,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 402, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages });
-    }, 3000);
+    this.socket = new WebSocket("ws:localhost:3001", "protocolOne");
   }
 
   render() {
+    console.log(this.socket);
     return (
       <div>
         <MessageList messages={this.state.messages} />
