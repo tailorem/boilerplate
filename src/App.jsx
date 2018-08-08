7cf431061d95743dd1f8a4fb82d65ce3f2c9809a
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-07 18:57:52 -0700

    edits currentUser

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ class App extends Component {
     super(props);
     this.state = {
       loading: true,
-      currentUser: false,
+      currentUser: false, /* {name: "notBob"} */
       messages,
       // currentUser: {name: "Taylour"},
     };
